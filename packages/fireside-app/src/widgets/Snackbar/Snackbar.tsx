86807e83955ee5e426cb8f3fb6474a1c89fed46f
manuelJung <manu.jung@relaxcommerce.com>
2020-04-17 23:09:12 +0200

    #7 basic styling for snackbar

--- packages/fireside-app/src/widgets/Snackbar/Snackbar.tsx
+++ packages/fireside-app/src/widgets/Snackbar/Snackbar.tsx
@@ -8,10 +8,11 @@ export default function Snackbar () {
   return (
     <Wrapper className='Snackbar'>
       {messages.data.map(msg => (
-        <div className='message'>
+        <Message type={msg.type}>
           <h5>{msg.title}</h5>
           <p>{msg.content}</p>
-        </div>
+
+        </Message>
       ))}
     </Wrapper>
   )
@@ -20,12 +21,38 @@ export default function Snackbar () {
 const Wrapper = styled.div`
   position: absolute;
   left: 0;
+  width: 100%;
+  max-width: 400px;
   bottom: 0;
   z-index: 10;
+`
 
-  > .message {
-    border: 1px solid black;
-    margin: 10px;
-    background: white;
+const Message = styled.div`
+  border-left: 15px solid ${(props:any) => {
+    if(props.type === 'error') return '#ec1f10'
+    if(props.type === 'info') return '#03a9f4'
+    if(props.type === 'warning') return '#ff9800'
+  }};
+  width: 100%;
+  margin: 10px;
+  background: #607d8b;
+  color: white;
+  padding: 10px;
+  > h5 {
+    margin: 0;
+  }
+  > p {
+    margin: 0;
   }
 `
+
+/*
+{
+type: 'snackbar/ADD_MESSAGE',
+payload: {
+type: 'error',
+title: 'snackbar title',
+content: 'snackbar content'
+}
+}
+*/

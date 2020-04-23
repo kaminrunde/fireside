ab4badf94a5827defee6afde49db6bc7346749bb
manuelJung <manu.jung@relaxcommerce.com>
2020-04-23 21:40:05 +0200

    init header

--- packages/fireside-app/src/App.tsx
+++ packages/fireside-app/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import {Router} from '@reach/router'
 import Snackbar from 'widgets/Snackbar'
 import {history} from './store'
+import Header from 'widgets/Header'
 
 import IndexRoute from 'routes/Index'
 import GridRoute from 'routes/Grid'
@@ -10,6 +11,7 @@ import GridRoute from 'routes/Grid'
 export default function App() {
   return (
     <div className="App">
+      <Header/>
       <Router history={history}>
         <IndexRoute path='/'/>
         <GridRoute path='/grid/:mediaSize' mediaSize=''/>
---
+++ packages/fireside-app/src/widgets/Header/Header.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+export default function Header () {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  height: 60px;
+  background: steelblue;
+`

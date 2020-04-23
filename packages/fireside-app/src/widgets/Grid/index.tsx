d04d39ce7857d5cb6e293163146b236fba44ee75
manuelJung <manu.jung@relaxcommerce.com>
2020-04-23 18:30:49 +0200

    added grid error boundary

---
+++ packages/fireside-app/src/widgets/Grid/index.tsx
@@ -0,0 +1,33 @@
+import Grid from './Grid'
+import React from 'react'
+import styled from 'styled-components'
+
+export default class GridWrapper extends React.Component {
+  state = {error:null}
+  componentDidCatch(e:any){
+    console.log(e)
+    this.setState({error:e})
+  }
+  render(){
+    if(this.state.error){
+      return (
+        <Wrapper>
+          <p>an error happened</p>
+          <button onClick={() => this.setState({error:null})}>reload</button>
+        </Wrapper>
+      )
+    }
+    return <Grid/>
+  }
+}
+
+const Wrapper = styled.div`
+  text-align: center;
+  > p {
+    font-size: 30px;
+  }
+  > button {
+    padding: 10px;
+    font-size: 30px;
+  }
+`

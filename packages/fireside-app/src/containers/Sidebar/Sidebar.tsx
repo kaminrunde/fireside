7cb65476b432c9fcdfe4b067379ae5ea6203c93d
manuelJung <manu.jung@relaxcommerce.com>
2020-08-10 18:20:02 +0200

    added Sidebar icons

--- packages/fireside-app/src/containers/Sidebar/Sidebar.tsx
+++ packages/fireside-app/src/containers/Sidebar/Sidebar.tsx
@@ -4,6 +4,8 @@ import {useSidebar} from 'modules/ui'
 import Link from 'components/Link'
 import config from 'config'
 import {useActiveMediaSizes} from 'modules/settings'
+import MediaIcon from 'components/MediaIcon'
+import {FaListUl, FaCog} from 'react-icons/fa'
 
 export default function Sidebar () {
   const sidebar = useSidebar()
@@ -14,17 +16,17 @@ export default function Sidebar () {
         <div key='overlay' className='overlay' onClick={sidebar.close}/>,
         <div key='content' className='content'>
           <Link className='item' to='/'>
-            <div className='icon'></div>
+            <div className='icon'><FaListUl/></div>
             <div className='label'>COMPONENTS</div>
           </Link>
           {config.mediaSizes.filter(row => ms.data[row.key]).map(ms => (
             <Link key={ms.key} className='item' to={`/grid/${ms.key}`}>
-              <div className='icon'></div>
+              <div className='icon'><MediaIcon icon={ms.icon}/></div>
               <div className='label'>{ms.label}</div>
             </Link>
           ))}
           <Link className='item' to='/settings'>
-            <div className='icon'></div>
+            <div className='icon'><FaCog/></div>
             <div className='label'>SETTINGS</div>
           </Link>
         </div>
@@ -55,13 +57,18 @@ const Wrapper = styled.div`
     background: white;
 
     > .item {
-      display: block;
+      display: flex;
       padding: 10px;
       text-decoration: none;
       color: black;
       cursor: pointer;
       font-family: 'Open Sans', sans-serif;
       &:hover {background: lightgrey;}
+      > .icon {
+        height:20px; width: 30px;
+        > svg {font-size: 20px;}
+      }
+      > .label {line-height: 20px;}
     }
   }
 `

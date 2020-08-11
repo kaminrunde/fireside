feef1e7f12ed756595811737d6d1799d86e44cbb
manuelJung <manu.jung@relaxcommerce.com>
2020-08-11 13:32:15 +0200

    init demo plugin

---
+++ packages/fireside-app/src/plugins/full-width-components.tsx
@@ -0,0 +1,11 @@
+import {FaArrowsAltH} from 'react-icons/fa'
+
+export default ctx => ({
+  name: 'full-width-components',
+  Badge: FaArrowsAltH,
+  componentContext: {
+    // BadgeSvg: (props) => props.
+    
+  }
+})
+

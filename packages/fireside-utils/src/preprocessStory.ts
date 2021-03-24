eda115a72ff78c912d5cf99a48320cbb40ac5b3f
manuelJung <manu.jung@relaxcommerce.com>
2021-03-24 22:54:37 +0100

    bugfixed

--- packages/fireside-utils/src/preprocessStory.ts
+++ packages/fireside-utils/src/preprocessStory.ts
@@ -4,9 +4,9 @@ import formatGrid from './formatGrid'
 import createComponentGridContexts from './createComponentGridContexts'
 import versionUpdate from './versionUpdate'
 
-export default async function preprocessStory (story:t.RawStory, config:t.Config) {
+export default async function preprocessStory(story: t.RawStory, config: t.Config) {
   story = versionUpdate(story)
-  const formatted:t.FormattedStory = {
+  const formatted: t.FormattedStory = {
     hash: story.hash,
     componentsById: {},
     allComponents: story.allComponents,
@@ -15,18 +15,18 @@ export default async function preprocessStory (story:t.RawStory, config:t.Config
     plugins: story.plugins || {}
   }
 
-  const usedComponentIds:Record<string,boolean> = {}
-  for(const ms in story.grids) {
-    for(const row of story.grids[ms].grid) {
-      for(const id of row) {
+  const usedComponentIds: Record<string, boolean> = {}
+  for (const ms in story.grids) {
+    for (const row of story.grids[ms].grid) {
+      for (const id of row) {
         usedComponentIds[id] = true
       }
     }
   }
 
-  let cachedGridContexts:ReturnType<typeof createComponentGridContexts>
-  const getGridContexts = (id:string) => {
-    if(!cachedGridContexts) cachedGridContexts = createComponentGridContexts(story)
+  let cachedGridContexts: ReturnType<typeof createComponentGridContexts>
+  const getGridContexts = (id: string) => {
+    if (!cachedGridContexts) cachedGridContexts = createComponentGridContexts(story)
     return cachedGridContexts[id]
   }
 
@@ -40,14 +40,13 @@ export default async function preprocessStory (story:t.RawStory, config:t.Config
   )
 
 
-  formattedComponents.forEach(([c,events],i) => {
-    const id = story.allComponents[i]
-    formatted.componentsById[id] = c
+  formattedComponents.forEach(([c, events], i) => {
+    formatted.componentsById[c.id] = c
     formatted.events.push(...events)
   })
 
-  const gridAreaDict:Record<string,string> = {}
-  for(let id in story.componentsById) gridAreaDict[id] = story.componentsById[id].props.gridArea
+  const gridAreaDict: Record<string, string> = {}
+  for (let id in story.componentsById) gridAreaDict[id] = story.componentsById[id].props.gridArea
 
   Object.entries(story.grids)
     .map(([key, val]) => [key, formatGrid(val, {

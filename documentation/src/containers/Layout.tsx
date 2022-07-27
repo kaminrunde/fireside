01c7abb5a09dacf940517b8c72df49c87eed7745
manuelJung <manuel.jung.wwi12@gmail.com>
2022-07-27 23:57:33 +0200

    bugfixed

--- documentation/src/containers/Layout.tsx
+++ documentation/src/containers/Layout.tsx
@@ -11,7 +11,7 @@ export default () => function Layout (props:{children:any}) {
       <Helmet>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
       </Helmet>
       <header />
       <div className='content'>
--- documentation/src/containers/MDXStyle.ts
+++ documentation/src/containers/MDXStyle.ts
@@ -3,6 +3,5 @@ import styled from 'styled-components'
 export default styled.div`
   h1 {
     margin-top: 0;
-    
   }
 `
